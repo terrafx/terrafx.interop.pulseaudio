6fef7a9822427e76e2e5155ee8f657996dc2c4dd
Tanner Gooding <tagoo@outlook.com>
2019-09-24 09:12:57 -0700

    Adding a DllImportResolver

--- sources/Interop/PulseAudio/Pulse.cs
+++ sources/Interop/PulseAudio/Pulse.cs
@@ -1,7 +1,28 @@
+// Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
 namespace TerraFX.Interop
 {
     public static unsafe partial class Pulse
     {
-        private const string libraryPath = "pulse";
+        private const string libraryPath = "libpulse";
+
+        static Pulse()
+        {
+            NativeLibrary.SetDllImportResolver(Assembly.GetExecutingAssembly(), ResolveLibrary);
+        }
+
+        private static IntPtr ResolveLibrary(string libraryName, Assembly assembly, DllImportSearchPath? searchPath)
+        {
+            if (!NativeLibrary.TryLoad("libpulse.so", assembly, searchPath, out var nativeLibrary))
+            {
+                nativeLibrary = NativeLibrary.Load("libpulse.so.0", assembly, searchPath);
+            }
+
+            return nativeLibrary;
+        }
     }
 }
